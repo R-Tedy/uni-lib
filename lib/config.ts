3863f71c18a348b94ebc3a4fd04130d43920e9d9
Roland Stedy <stedyroland@gmail.com>
2025-04-26 08:29:40 +0300

    set up email example

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const config = {
   env : {
     apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    publicApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
     imagekit: {
       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
@@ -15,5 +16,5 @@ const config = {
     }
   }
 }
-
+ 
 export default config;
