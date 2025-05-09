efecb0d0ac6bdd21012c8f6ebd1657016a54a6a0
R-Tedy <stedyroland2@gmail.com>
2025-05-09 17:41:14 +0300

    breath

---
+++ components/FileUpload.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Imagekit from 'imagekit';
+import { useState } from 'react';
+
+const FileUpload = () => {
+  return (
+    <div>FileUpload</div>
+  )
+}
+
+export default FileUpload
